dc402278503599492b184359355a0cbc872964a8
Steph <you@example.com>
2024-06-12 19:33:19 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,11 @@ public class Main {
             if (input.equals("exit 0")) {
                 // Exit the program with status 0
                 System.exit(0);
+            } else if (input.startsWith("echo ")) {
+                // Handle the echo command
+                // Extract and print the text following "echo "
+                String textToEcho = input.substring(5);
+                System.out.println(textToEcho);
             } else {
                 // Handle unrecognized commands
                 System.out.println(input + ": command not found");
