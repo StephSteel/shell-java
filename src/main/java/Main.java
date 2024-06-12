c7e638d161e08579b378572cc6af592218833fd3
Steph Steel <stephsteel8@gmail.com>
2024-06-12 19:12:52 +0300

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        System.out.println("Logs from your program will appear here!");
+        // Uncomment this block to pass the first stage
         System.out.print("$ ");
-        
-
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
     }
