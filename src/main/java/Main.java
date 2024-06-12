f010b07908149a8c2ea1b10b834caf7eed1b20ea
Steph Steel <stephsteel8@gmail.com>
2024-06-12 19:07:33 +0300

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,12 @@
-// Uncomment this block to pass the first stage
-// import java.util.Scanner;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("$ ");
-        System.out.print("Logs from your program will appear here!");
+
+        System.out.println("Logs from your program will appear here!");
+        System.out.print("$ ");
         
-        // Uncomment this block to pass the first stage
-        // System.out.print("$ ");
-        //
-        // Scanner scanner = new Scanner(System.in);
-        // String input = scanner.nextLine();
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
     }
