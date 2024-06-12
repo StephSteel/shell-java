6c220b6f179069143d07b41a95acb0d784cd3210
Steph <you@example.com>
2024-06-12 19:27:46 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Print the prompt
-        System.out.print("$ ");
-        // Create a scanner to read user input
         Scanner scanner = new Scanner(System.in);
-        // Read the input from the user
-        String input = scanner.nextLine();
-        
-        // For this stage, assume no command is recognized
-        // Print the error message for the unrecognized command
-        System.out.println(input + ": command not found");
+
+        while (true) {
+            // Print the prompt
+            System.out.print("$ ");
+            // Read the input from the user
+            String input = scanner.nextLine();
+            
+            // For this stage, assume no command is recognized
+            // Print the error message for the unrecognized command
+            System.out.println(input + ": command not found");
+        }
     }
 }
